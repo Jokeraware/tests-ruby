5b34d553ce04434e888b5b8902c71e6fc4d3a8e4
Jokeraware <benedicte.tran@hotmail.com>
2024-01-23 10:43:09 +0100

    modif et ajout exo

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -2,19 +2,19 @@ def translate (word)
     vowels = %w[a e i o u y]
     # alphabet = ("a".."z").to_a #transforme en array
     # consonant = alphabet - vowels
-    consonant_exception = %w[qu sch]
+    consonant_exception = %w[qu ch sch squ th thr br] 
     # n = word.split #.split crée une array
 
     words = word.split.map do |word|
         if vowels.include?(word[0]) #prend la premiere lettre, si vowels ajoute ay
             word + "ay"
-        elsif  consonant_exception.include?(word[0..1]) #si 1er et 2eme lettre = qu ou sch
-            prefix = word[0..1]
-            rest_char = word[2..-1]
-            rest_char + prefix + "ay"
         elsif consonant_exception.include?(word[0..2])
             prefix = word[0..2]
             rest_char = word[3..-1]
+            rest_char + prefix + "ay"
+        elsif  consonant_exception.include?(word[0..1]) #si 1er et 2eme lettre = qu ou sch
+            prefix = word[0..1]
+            rest_char = word[2..-1]
             rest_char + prefix + "ay"
         else 
             first_char = word[0]
